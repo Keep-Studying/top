1edd2820703f5a353140c2ebbd5c4b628ebe5bbf
study <keep_study_66@163.com>
2020-09-05 22:33:30 +0800

    refactor(juc):change some ThreadPool codes

--- juc/src/main/java/com/study/juc/pool/ThreadPool.java
+++ juc/src/main/java/com/study/juc/pool/ThreadPool.java
@@ -3,9 +3,8 @@
  */
 package com.study.juc.pool;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.concurrent.*;
 
 /**
  * 线程池ThreadPoolExecutor
@@ -13,7 +12,7 @@ import java.util.concurrent.TimeUnit;
  * @version : ThreadPool.java, v 0.1 2020年09月01日 23:39 study Exp $
  */
 public class ThreadPool {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         /*for (int i=0;i<300;i++){
             new Thread(new Runnable() {
                     @Override
@@ -37,14 +36,38 @@ public class ThreadPool {
          * RocketMQ，Eureka，Nacos等中间件，大多都是用的是：2*CPU核数
          * 最佳线程数=CPU核数*[1+(IO耗时/CPU耗时)]
          * */
+        /**
+         * 输出内容如下：
+         * submit method,i am task ：pool-1-thread-1
+         * 0
+         * submit method,i am task ：pool-1-thread-1
+         * submit method,i am task ：pool-1-thread-2
+         * 1
+         * submit method,i am task ：pool-1-thread-3
+         * 2
+         * submit method,i am task ：pool-1-thread-4
+         * 3
+         * submit method,i am task ：pool-1-thread-5
+         * 4
+         * 5
+         * */
         ThreadPoolExecutor threadPoolExecutor =
                 new ThreadPoolExecutor(5,10,5000,
                         TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
 
+        ArrayList<Future> futures = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
-            threadPoolExecutor.submit(()->{
-                System.out.println("i am task ："+Thread.currentThread().getName());
-            },i);
+            //execute是无返回值的方式
+            /*threadPoolExecutor.execute(()->{
+                System.out.println("execute method,i am task ："+Thread.currentThread().getName());
+            });*/
+            //submit是有返回值的
+            futures.add(threadPoolExecutor.submit(()->{
+                System.out.println("submit method,i am task ："+Thread.currentThread().getName());
+            },i));
+        }
+        for (Future future : futures) {
+            System.out.println(future.get());
         }
 
         threadPoolExecutor.shutdown();  //running->shutdown
